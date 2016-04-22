af2c214cb6257491ea32e0be2dab449098fc557b
Rodrigo Hernandez <kwizatz@aeongames.com>
2016-04-22 16:46:30 -0600

    Initial Code Dump
    I'll be using Protocol Buffers for most data files, starting by
    configurations file.
    
    This is my Vulkan sandbox, so related CMake files have been added.

---
+++ include/aeongames/Platform.h
@@ -0,0 +1,35 @@
+/*
+Copyright 2016 Rodrigo Jose Hernandez Cordoba
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#ifndef AEONGAMES_PLATFORM_H
+#define AEONGAMES_PLATFORM_H
+#ifndef DLL
+#if defined(WIN32)
+#ifdef DLL_EXPORT
+#define DLL __declspec( dllexport )
+#else
+#define DLL __declspec( dllimport )
+#endif
+#else
+#define DLL
+#endif
+#endif
+#endif
+
+#if defined(MINGW32) || defined(MINGW64)
+#define ENTRYPOINT __attribute__((force_align_arg_pointer))
+#else
+#define ENTRYPOINT
+#endif
