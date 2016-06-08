c8f391bcfcac8c9a087d0f32b64c2521e28f530c
Rodrigo Hernandez <kwizatz@aeongames.com>
2016-06-08 17:25:10 -0600

    Added initial files for material class and PB.

---
+++ engine/renderers/opengl/Material.cpp
@@ -0,0 +1,58 @@
+/*
+Copyright 2016 Rodrigo Jose Hernandez Cordoba
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include <fstream>
+#include <sstream>
+#include <ostream>
+#include <regex>
+#include "Material.h"
+#include "OpenGLFunctions.h"
+
+#ifdef _MSC_VER
+#pragma warning( push )
+#pragma warning( disable : 4251 )
+#endif
+#include <google/protobuf/text_format.h>
+#include "material.pb.h"
+#ifdef _MSC_VER
+#pragma warning( pop )
+#endif
+
+namespace AeonGames
+{
+Material::Material ( const std::string& aFilename ) try :
+        mFilename ( aFilename )
+    {
+        Initialize();
+    }
+    catch ( ... )
+    {
+        Finalize();
+        throw;
+    }
+
+    Material::~Material()
+    {
+    }
+
+    void Material::Initialize()
+    {
+        static MaterialBuffer material_buffer;
+    }
+
+    void Material::Finalize()
+    {
+    }
+}
