5095a7d60d2f97f5e15c455f3ddd980a1432d04a
Rodrigo Hernandez <kwizatz@aeongames.com>
2016-06-09 11:27:03 -0600

    Added code to make the QWindow based EngineWindow request its own DC.
    Windows specific.

--- tools/modelviewer/EngineWindow.cpp
+++ tools/modelviewer/EngineWindow.cpp
@@ -18,7 +18,24 @@ namespace AeonGames
 {
     EngineWindow::EngineWindow ( QWindow *parent ) : QWindow ( parent )
     {
+        /* On
+        Windows we want the window to own its device context.
+        This code works on Qt 5.6.0, but if it does not
+        or a new non Qt application needs to set it,
+        a window class style seems to be mutable by using
+        SetClassLongPtr( ..., GCL_STYLE, ... );
+        instead.
+        https://msdn.microsoft.com/en-us/library/windows/desktop/ms633589%28v=vs.85%29.aspx
+        http://www.gamedev.net/topic/319124-ogl-and-windows/#entry3055268
 
+        This flag is Windows specific, so Linux or Mac implementations
+        probably just ignore it, I have not verified this.
+        */
+        auto current_flags = flags();
+        if ( ! ( current_flags & Qt::MSWindowsOwnDC ) )
+        {
+            setFlags ( current_flags | Qt::MSWindowsOwnDC );
+        }
     }
     EngineWindow::~EngineWindow()
     {
