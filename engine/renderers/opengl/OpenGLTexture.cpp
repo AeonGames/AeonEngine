1040a181e30ad7103e01ca01efe99fe757892537
Rodrigo Hernandez <kwizatz@aeongames.com>
2016-10-18 16:44:17 -0600

    Added implementation of OpenGL Texture class
    This code is untested, but should work
    for the most part.
    
    This first implementation makes a lot
    of assumptions which may not always be true,
    however, it should work fine for the time
    being.

--- engine/renderers/opengl/OpenGLTexture.cpp
+++ engine/renderers/opengl/OpenGLTexture.cpp
@@ -13,32 +13,46 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <fstream>
-#include <sstream>
-#include <ostream>
-#include <regex>
-#include "aeongames/ProtoBufClasses.h"
-#include "ProtoBufHelpers.h"
-#ifdef _MSC_VER
-#pragma warning( push )
-#pragma warning( disable : 4251 )
-#endif
-#include "material.pb.h"
-#ifdef _MSC_VER
-#pragma warning( pop )
-#endif
 
 #include "OpenGLFunctions.h"
 #include "OpenGLTexture.h"
+#include "aeongames/Image.h"
+#include "aeongames/Utilities.h"
 
 namespace AeonGames
 {
     OpenGLTexture::OpenGLTexture ( const std::string& aFilename ) :
         mFilename ( aFilename )
     {
+        auto image = GetImage ( GetFileExtension ( mFilename ), mFilename );
+        glGenTextures ( 1, &mTexture );
+        OPENGL_CHECK_ERROR_THROW;
+        glBindTexture ( GL_TEXTURE_2D, mTexture );
+        OPENGL_CHECK_ERROR_THROW;
+        /**@todo Write a format/type dictionary?
+            These guesses only work for PNG at the moment.*/
+        glTexImage2D ( GL_TEXTURE_2D,
+                       0,
+                       ( image->Format() == Image::ImageFormat::RGB ) ? GL_RGB : GL_RGBA,
+                       image->Width(),
+                       image->Height(),
+                       0,
+                       ( image->Format() == Image::ImageFormat::RGB ) ? GL_RGB : GL_RGBA,
+                       ( image->Type() == Image::ImageType::UNSIGNED_BYTE ) ? GL_UNSIGNED_BYTE : GL_UNSIGNED_SHORT,
+                       image->Data() );
+        OPENGL_CHECK_ERROR_THROW;
+        glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+        OPENGL_CHECK_ERROR_THROW;
+        glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+        OPENGL_CHECK_ERROR_THROW;
     }
 
     OpenGLTexture::~OpenGLTexture()
     {
+        if ( glIsTexture ( mTexture ) == GL_TRUE )
+        {
+            glDeleteTextures ( 1, &mTexture );
+        }
+        mTexture = 0;
     }
 }
