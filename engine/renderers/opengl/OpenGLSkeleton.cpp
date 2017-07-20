3c9fe79783aa0039aa35e827ad3ed451778acab0
Rodrigo Hernandez <kwizatz@aeongames.com>
2017-07-19 23:24:53 -0600

    Linux build fix.

--- engine/renderers/opengl/OpenGLSkeleton.cpp
+++ engine/renderers/opengl/OpenGLSkeleton.cpp
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include <cassert>
+#include <cstring>
 #include <vector>
 #include "aeongames/Skeleton.h"
 #include "aeongames/ResourceCache.h"
