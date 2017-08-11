85e7eea30b82ab5d1ca8a88c70a10436bba8b40a
Rodrigo Hernandez <kwizatz@aeongames.com>
2017-08-11 16:38:53 -0600

    Wrapping our home brew make_unique implementation...
    into the AeonGames namespace to avoid collisions with other
    implementations.

--- include/aeongames/Memory.h
+++ include/aeongames/Memory.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 Rodrigo Jose Hernandez Cordoba
+Copyright (C) 2016,2017 Rodrigo Jose Hernandez Cordoba
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,12 +18,15 @@ limitations under the License.
 #include <memory>
 #if __cplusplus < 201300L && __cplusplus >= 201103L
 // Taken from EMC++ Item 21
-namespace std
+namespace AeonGames
 {
-    template<typename T, typename... Ts>
-    std::unique_ptr<T> make_unique ( Ts&&... params )
+    namespace std
     {
-        return std::unique_ptr<T> ( new T ( std::forward<Ts> ( params )... ) );
+        template<typename T, typename... Ts>
+        std::unique_ptr<T> make_unique ( Ts&&... params )
+        {
+            return std::unique_ptr<T> ( new T ( std::forward<Ts> ( params )... ) );
+        }
     }
 }
 #endif
