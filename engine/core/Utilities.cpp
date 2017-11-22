8fcc024a645a3e0e995bc47491475e0d2418aa35
Rodrigo Hernandez <kwizatz@aeongames.com>
2017-11-22 16:47:39 -0600

    Changes to build with MSYS2.
    Linux build may be broken.

--- engine/core/Utilities.cpp
+++ engine/core/Utilities.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 Rodrigo Jose Hernandez Cordoba
+Copyright (C) 2016,2017 Rodrigo Jose Hernandez Cordoba
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ namespace AeonGames
 
     bool FileExists ( const std::string& aFilePath )
     {
-        struct stat stat_buffer;
-        return ( stat ( aFilePath.c_str(), &stat_buffer ) == 0 ) ? true : false;
+        struct stat stat_buffer {};
+        return ( ::stat ( aFilePath.c_str(), &stat_buffer ) == 0 ) ? true : false;
     }
 }
