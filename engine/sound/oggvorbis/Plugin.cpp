9040a9848e25e39323120465b6051c1882de236f
Rodrigo Hernandez <kwizatz@aeongames.com>
2017-12-08 18:44:44 -0600

    Added initial core sound system code.

--- engine/sound/oggvorbis/Plugin.cpp
+++ engine/sound/oggvorbis/Plugin.cpp
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-/** \File Implements the interface for the PNG plugin.*/
+/** \File Implements the interface for the Ogg plugin.*/
 #include "aeongames/Platform.h"
 #include "aeongames/Plugin.h"
 #include "aeongames/Memory.h"
