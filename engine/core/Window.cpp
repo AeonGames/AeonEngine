10e260bdcfb1f764068667e36f01dfe6b8ae4f51
Rodrigo Hernandez <kwizatz@aeongames.com>
2017-12-18 19:15:17 -0600

    Added Window class core implementation.

--- engine/core/Window.cpp
+++ engine/core/Window.cpp
@@ -13,24 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#ifndef AEONGAMES_WINDOW_H
-#define AEONGAMES_WINDOW_H
-#include "Memory.h"
-#include "aeongames/Matrix4x4.h"
-#include "aeongames/Transform.h"
-#include "aeongames/Platform.h"
+#include "aeongames/Window.h"
 namespace AeonGames
 {
-    class Scene;
-    class Window
-    {
-    public:
-        DLL virtual ~Window();
-        virtual void ResizeViewport ( uint32_t aWidth, uint32_t aHeight ) = 0;
-        virtual void Render ( const std::shared_ptr<const Scene>& aScene ) const = 0;
-    protected:
-        Matrix4x4 mProjectionMatrix{};
-        Transform mViewTransform{};
-    };
+    Window::~Window() {}
 }
-#endif
+
