8dcf1533e055cc711820b547e4b83fb144ac0f84
Rodrigo Hernandez <kwizatz@aeongames.com>
2018-01-29 15:14:32 -0600

    Templatized Dependency Map class,
    And some other to do work.
    
    When an initializer list constructor is needed,
    refer to previous commit for reference.

--- engine/include/DependencyMap.h
+++ engine/include/DependencyMap.h
@@ -21,22 +21,31 @@ limitations under the License.
 #include <algorithm>
 #include <exception>
 #include <limits>
+#include <functional>
+
 namespace AeonGames
 {
     /**
-    @todo Add const only operator []
     @todo Add iterators and begin and end functions.
-    @todo Turn into a template.
     */
+    template <
+        class Key,
+        class T,
+        class Hash = std::hash<Key>,
+        class KeyEqual = std::equal_to<Key>,
+        class MapAllocator = std::allocator< std::pair<const Key, T>>,
+        class VectorAllocator = std::allocator<Key>
+        >
     class DependencyMap
     {
         /*
         Node Data Contents:
         std::tuple<
-            size_t,             //-> Parent Iterator
-            size_t,             //-> Child Iterator
-            size_t,             //-> Visit Mark
-            std::vector<size_t> //-> Dependencies
+            Key,                 //-> Parent Iterator
+            size_t,              //-> Child Iterator
+            size_t,              //-> Visit Mark
+            std::vector<size_t>, //-> Dependencies
+            T                    //-> Payload
             >
         */
     public:
@@ -49,22 +58,29 @@ namespace AeonGames
             sorted.reserve ( count );
         }
 
-        void insert ( const std::tuple<size_t, std::vector<size_t>>& item )
+        void insert ( const std::tuple<Key, std::vector<Key>, T>& item )
         {
+            if ( sorted.empty() && graph.empty() )
+            {
+                graph[std::get<0> ( item )] = {{}, 0, 0, std::get<1> ( item ), std::get<2> ( item ) };
+                sorted.emplace_back ( std::get<0> ( item ) );
+                return;
+            }
             ///@todo find a way not to reset marks.
             for ( auto& i : graph )
             {
+                //std::get<0> ( i.second ) = {};
+                //std::get<1> ( i.second ) = 0;
                 std::get<2> ( i.second ) = 0;
             }
+            std::size_t index = 0;
             ///@todo find a way not to insert the item until it is found to be valid.
-            graph[std::get<0> ( item )] = {0, 0, 0, std::get<1> ( item ) };
-            ///@todo find a way not to clear sorted but just insert the new item instead.
-            sorted.clear();
+            graph[std::get<0> ( item )] = {{}, 0, 0, std::get<1> ( item ), std::get<2> ( item ) };
             for ( auto& i : graph )
             {
                 if ( std::get<2> ( i.second ) == 0 )
                 {
-                    auto node = i.first;
+                    Key node = i.first;
                     while ( true )
                     {
                         if ( std::get<1> ( graph[node] ) < std::get<3> ( graph[node] ).size() )
@@ -78,20 +94,27 @@ namespace AeonGames
                                 node = ( *next ).first;
                                 if ( std::get<2> ( graph.at ( node ) ) == 1 )
                                 {
+                                    ///@todo If there is no way to insert the new node, leave the map in a usable state.
                                     throw std::runtime_error ( "New node would create a circular dependency." );
                                 }
                             }
                         }
                         else
                         {
+                            std::cout << node << "(" << ( ( sorted[index] == node ) ? "old" : "new" ) << ")" << ", ";
                             std::get<2> ( graph.at ( node ) ) = 2;
-                            sorted.emplace_back ( node );
-                            std::cout << node << ", ";
+                            if ( sorted[index] != node )
+                            {
+                                sorted.insert ( sorted.begin() + index, node );
+                                //return;
+                            }
+                            ++index;
                             std::get<1> ( graph[node] ) = 0; // Reset counter for next traversal.
                             if ( node == i.first )
                             {
                                 break;
                             }
+                            // Go back to the parent
                             node = std::get<0> ( graph[node] );
                         }
                     }
@@ -99,34 +122,147 @@ namespace AeonGames
             }
             std::cout << std::endl;
         }
+        void erase ( const Key& key )
+        {
+            graph.erase ( key );
+            sorted.erase ( std::remove ( sorted.begin(), sorted.end(), key ), sorted.end() );
+        }
+        const T& operator[] ( const std::size_t index ) const
+        {
+            if ( index >= sorted.size() )
+            {
+                throw std::out_of_range ( "Index out of range." );
+            }
+            return std::get<4> ( graph.at ( sorted[index] ) );
+        }
+        const std::size_t size() const
+        {
+            return sorted.size();
+        }
     private:
         /**
         @todo Find out if it is posible to remove marks from storage.
         @todo Add User Data field.
         */
-        std::unordered_map <
-        size_t,
+        std::unordered_map
+        <
+        Key,
         std::tuple <
-        size_t,                 //-> Parent Iterator
-        size_t,                 //-> Child Iterator
-        size_t,                 //-> Visit Mark
-        std::vector<size_t >>   //-> Dependencies
+        Key,                              //-> Parent Iterator
+        size_t,                           //-> Child Iterator
+        size_t,                           //-> Visit Mark
+        std::vector<Key, VectorAllocator>, //-> Dependencies
+        T >,                              //-> Payload
+        Hash,                             //-> Hash function
+        KeyEqual,
+        MapAllocator
         > graph;
-        std::vector<size_t> sorted;
+        std::vector<Key, VectorAllocator> sorted;
     };
 }
+
 int main ( int argc, char **argv )
 {
-    AeonGames::DependencyMap dv;
+#if 0
+    AeonGames::DependencyMap<size_t, std::function<void() >> dv;
     dv.reserve ( 10 );
-    dv.insert ( {6, {7}} );
-    dv.insert ( {1, {2, 3}} );
-    dv.insert ( {3, {4, 5}} );
-    dv.insert ( {5, {4}} );
-    dv.insert ( {9, {7}} );
-    dv.insert ( {2, {}} );
-    dv.insert ( {4, {}} );
-    dv.insert ( {7, {1, 2, 3, 4, 5}} );
-    dv.insert ( {8, {9}} );
+    dv.insert ( {6, {7}, []()
+    {
+        std::cout << 6 << std::endl;
+    }
+                } );
+    dv.insert ( {1, {2, 3}, []()
+    {
+        std::cout << 1 << std::endl;
+    }
+                } );
+    dv.insert ( {3, {4, 5}, []()
+    {
+        std::cout << 3 << std::endl;
+    }
+                } );
+    dv.insert ( {5, {4}, []()
+    {
+        std::cout << 5 << std::endl;
+    }
+                } );
+    dv.insert ( {9, {7}, []()
+    {
+        std::cout << 9 << std::endl;
+    }
+                } );
+    dv.insert ( {2, {}, []()
+    {
+        std::cout << 2 << std::endl;
+    }
+                } );
+    dv.insert ( {4, {}, []()
+    {
+        std::cout << 4 << std::endl;
+    }
+                } );
+    dv.insert ( {7, {1, 2, 3, 4, 5}, []()
+    {
+        std::cout << 7 << std::endl;
+    }
+                } );
+    dv.insert ( {8, {9}, []()
+    {
+        std::cout << 8 << std::endl;
+    }
+                } );
+#else
+    AeonGames::DependencyMap<std::string, std::function<void() >> dv;
+    dv.reserve ( 10 );
+    dv.insert ( {"six", {"seven"}, []()
+    {
+        std::cout << 6 << std::endl;
+    }
+                } );
+    dv.insert ( {"one", {"two", "three"}, []()
+    {
+        std::cout << 1 << std::endl;
+    }
+                } );
+    dv.insert ( {"three", {"four", "five"}, []()
+    {
+        std::cout << 3 << std::endl;
+    }
+                } );
+    dv.insert ( {"five", {"four"}, []()
+    {
+        std::cout << 5 << std::endl;
+    }
+                } );
+    dv.insert ( {"nine", {"seven"}, []()
+    {
+        std::cout << 9 << std::endl;
+    }
+                } );
+    dv.insert ( {"two", {}, []()
+    {
+        std::cout << 2 << std::endl;
+    }
+                } );
+    dv.insert ( {"four", {}, []()
+    {
+        std::cout << 4 << std::endl;
+    }
+                } );
+    dv.insert ( {"seven", {"one", "two", "three", "four", "five"}, []()
+    {
+        std::cout << 7 << std::endl;
+    }
+                } );
+    dv.insert ( {"eight", {"nine"}, []()
+    {
+        std::cout << 8 << std::endl;
+    }
+                } );
+#endif
+    for ( std::size_t i = 0; i < dv.size(); ++i )
+    {
+        dv[i]();
+    }
     return 0;
 }
