e39614bc2fb3c2b6d1c35b6486533f0e8e64e91d
Rodrigo Hernandez <kwizatz@aeongames.com>
2018-04-05 17:51:26 -0600

    Trying to fix Linux build.
    Can't reproduce the issue on my setup,
    so I asume its an issue of the older
    compiler at travis.
    
    Here the idea is to let the compiler know
    that the reference is to an abstract class
    before declaring it.

--- tools/worldeditor/Grid.cpp
+++ tools/worldeditor/Grid.cpp
@@ -13,8 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include "Grid.h"
 #include "aeongames/Renderer.h"
+#include "Grid.h"
 namespace AeonGames
 {
     Grid::Grid ( const Renderer& aRenderer ) : mRenderer{aRenderer}
