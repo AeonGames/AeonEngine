43f85f4d47c889d88653425b9e25dc47d6708fec
Rodrigo Hernandez <kwizatz@aeongames.com>
2018-04-25 17:46:24 -0600

    More work on FlyWeight class.

--- include/aeongames/FlyWeight.h
+++ include/aeongames/FlyWeight.h
@@ -17,7 +17,7 @@ limitations under the License.
 #define AEONGAMES_FLYWEIGHT_STORE_H
 
 #include <unordered_map>
-#include <functional>
+#include <mutex>
 
 namespace AeonGames
 {
@@ -28,16 +28,69 @@ namespace AeonGames
         FlyWeight() = default;
         FlyWeight ( const Key& aKey ) : mKey ( aKey )
         {
-            mStore[mKey] = this;
+            Pack ( aKey );
         }
         virtual ~FlyWeight ()
         {
-            mStore.erase ( mKey );
+            Unpack();
         }
         Key mKey{};
-        static std::unordered_map<Key, Value*> mStore;
+        static std::unordered_map<Key, FlyWeight<Key, Value>*> mStore;
+    public:
+        class Handle
+        {
+            const Key mKey{};
+        public:
+            Handle ( const Key& aKey ) : mKey{aKey}
+            {
+                if ( mKey == Key{} )
+                {
+                    throw std::runtime_error ( "The null/empty key is reserved for unpacked objects." );
+                }
+            }
+            const Value* const Get() const
+            {
+                auto it = FlyWeight<Key, Value>::mStore.find ( mKey );
+                if ( it != FlyWeight<Key, Value>::mStore.end() )
+                {
+                    return it->second;
+                }
+                return nullptr;
+            }
+            bool IsValid() const
+            {
+                return Get() != nullptr;
+            }
+            const Value* const operator->() const
+            {
+                auto result = Get();
+                if ( !result )
+                {
+                    throw std::runtime_error ( "Invalid FlyWeight Object." );
+                }
+                return result;
+            }
+        };
+        void Pack ( const Key& aKey )
+        {
+            if ( aKey == Key{} )
+            {
+                throw std::runtime_error ( "The null/empty key is reserved for unpacked objects." );
+            }
+            mKey = aKey;
+            static std::mutex m;
+            std::lock_guard<std::mutex> hold ( m );
+            mStore[mKey] = this;
+        }
+        void Unpack()
+        {
+            static std::mutex m;
+            std::lock_guard<std::mutex> hold ( m );
+            mStore.erase ( mKey );
+            mKey = Key{};
+        }
     };
     template <class Key, class Value>
-    std::unordered_map<Key, Value*> FlyWeight<Key, Value>::mStore{};
+    std::unordered_map<Key, FlyWeight<Key, Value>*> FlyWeight<Key, Value>::mStore{};
 }
 #endif
