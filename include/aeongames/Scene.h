942b3f488828034ba91ddc8bf47751630a90a374
Rodrigo Hernandez <kwizatz@aeongames.com>
2018-06-20 18:42:39 -0600

    Reverted Scene and Node clases.
    Turns out the refactor is too rigid to allow for
    customization, this is a new refactor starting from the
    shared pointer implementation, replacing smart pointers
    with raw pointers.
    
    Neither scene nor node classes own node pointers now,
    so a cache of node objects must be set aside as
    exemplified by the SceneModel class.

--- include/aeongames/Scene.h
+++ include/aeongames/Scene.h
@@ -15,202 +15,43 @@ limitations under the License.
 */
 #ifndef AEONGAMES_SCENE_H
 #define AEONGAMES_SCENE_H
-#include <cstdint>
-#include <string>
-#include <vector>
-#include <bitset>
-#include <functional>
-#include <limits>
-#include <unordered_map>
-#include <tuple>
+/*! \file
+    \brief Header for the Scene class.
+    \copy 2014-2018
+    \author Rodrigo Hernandez.
+*/
 #include "aeongames/Platform.h"
-#include "aeongames/Transform.h"
+#include "aeongames/Memory.h"
+#include <vector>
+#include <string>
+#include <functional>
 
 namespace AeonGames
 {
+    class Node;
+    class Renderer;
+    /*! \brief Scene class.
+      Scene is the container for all elements in a game level,
+      takes care of collision, rendering and updates to all elements therein.
+    */
     class Scene
     {
     public:
-        class Node
-        {
-        public:
-            DLL Node();
-            DLL Node ( const Node & aNode );
-            DLL Node ( const Node && aNode );
-            DLL Node& operator= ( const Node & aNode );
-            DLL Node& operator= ( const Node && aNode );
-            DLL Node ( std::initializer_list<Node> aList );
-            DLL ~Node();
-            ///@name Scene Functions
-            ///@{
-            /** Append a child node at the end of the child vector. */
-            DLL void Append ( const Node& aNode );
-            /** Insert a child node at the specified index on the child vector. */
-            DLL void Insert ( size_t aIndex, const Node& aNode );
-            /** Reparent a node at the specified index on this node's child vector. */
-            DLL void Move ( size_t aIndex, Node&& aNode );
-            /** Delete a child node given its index.
-             * This function deletes the child node and recursivelly all further descendants.
-             */
-            DLL void Erase ( std::vector<Node>::size_type aIndex );
-            /** Delete a child node given a reference to it.
-             * This function deletes the child node and recursivelly all further descendants.
-             * @note the provided reference MUST be to the specific node to be removed,
-             * passing a copy of the node will likely make the operation fail.
-             * @todo Decide if having iterators is a better idea to delete by reference.
-             */
-            DLL void Erase ( const Node& aNode );
-            /** Get the current size of the children node vector.
-             * This is the count of direct descendants.
-             */
-            DLL std::vector<Node>::size_type GetChildrenCount() const;
-            /** Get a const reference to a direct descendant by index.
-             * May throw an std::out_of_range exception.
-             */
-            DLL const Node& GetChild ( size_t aIndex ) const;
-            /** Get a eference to a direct descendant by index.
-             * May throw an std::out_of_range exception.
-             */
-            DLL Node& GetChild ( size_t aIndex );
-            /** Get a const reference to a direct descendant by index.
-             * Does NOT do bounds checking.
-             */
-            DLL const Node& operator[] ( const std::size_t aIndex ) const;
-            /** Get a eference to a direct descendant by index.
-             * Does NOT do bounds checking.
-             */
-            DLL Node& operator[] ( const std::size_t aIndex );
-            /** Get a const pointer to the node's parent.
-             * Returned pointer may be nullptr.
-             */
-            DLL const Node* GetParent() const;
-            /** Get a pointer to the node's parent.
-             * Returned pointer may be nullptr.
-             */
-            DLL Node* GetParent();
-            /** Get The index of the node in its parent or tree child node vector.
-             * If the parent is nullptr this function throws an std::runtime_error exception.
-             */
-            DLL std::size_t GetIndex() const;
-            /** Iterative depth first search iteration.
-            Iterates all descendants without recursion in pre-order.
-            This function guarrantees that parents are processed before their children.
-            @param aAction a function, function pointer or function like object to proccess each child node.
-            @sa Node::LoopTraverseDFSPostOrder,Node::RecursiveTraverseDFSPreOrder,Node::RecursiveTraverseDFSPostOrder
-            */
-            DLL void LoopTraverseDFSPreOrder ( const std::function<void ( Node& ) >& aAction );
-            /** Iterative depth first search iteration.
-            Iterates all descendants without recursion in pre-order.
-            This function guarrantees that parents are processed before their children.
-            This function does the same as the single argument version except that it runs two
-            functions, a preamble upon reaching a node and a postamble upon leaving it having run the preambles and postambles
-            of all of the node's children.
-            @param aPreamble a function, function pointer or function like object to run upon reaching each child node.
-            @param aPostamble a function, function pointer or function like object to run upon leaving each child node.
-            @sa Node::LoopTraverseDFSPostOrder,Node::RecursiveTraverseDFSPreOrder,Node::RecursiveTraverseDFSPostOrder
-            */
-            DLL void LoopTraverseDFSPreOrder (
-                const std::function<void ( Node& ) >& aPreamble,
-                const std::function<void ( Node& ) >& aPostamble );
-            /** Constant version of LoopTraverseDFSPreOrder. */
-            DLL void LoopTraverseDFSPreOrder ( const std::function<void ( const Node& ) >& aAction ) const;
-            /** Iterative depth first search iteration.
-            Iterates all descendants without recursion in post-order.
-            This function guarrantees that children are processed before their parent.
-            @param aAction a function, function pointer or function like object to proccess each child node.
-            @sa Node::LoopTraverseDFSPreOrder,Node::RecursiveTraverseDFSPreOrder,Node::RecursiveTraverseDFSPostOrder
-            */
-            DLL void LoopTraverseDFSPostOrder ( const std::function<void ( Node& ) >& aAction );
-            /** Constant version of LoopTraverseDFSPostOrder. */
-            DLL void LoopTraverseDFSPostOrder ( const std::function<void ( const Node& ) >& aAction ) const;
-            /** Recursive depth first search iteration.
-            Iterates all descendants with recursion in pre-order.
-            This function guarrantees that parents are processed before their children.
-            @param aAction a function, function pointer or function like object to proccess each child node.
-            @sa Node::LoopTraverseDFSPreOrder,Node::LoopTraverseDFSPostOrder,Node::RecursiveTraverseDFSPostOrder
-            */
-            DLL void RecursiveTraverseDFSPreOrder ( const std::function<void ( Node& ) >& aAction );
-            /** Recursive depth first search iteration.
-            Iterates all descendants with recursion in post-order.
-            This function guarrantees that children are processed before their parent.
-            @param aAction a function, function pointer or function like object to proccess each child node.
-            @sa Node::LoopTraverseDFSPreOrder,Node::LoopTraverseDFSPostOrder,Node::RecursiveTraverseDFSPreOrder
-            */
-            DLL void RecursiveTraverseDFSPostOrder ( const std::function<void ( Node& ) >& aAction );
-
-            DLL void LoopTraverseAncestors ( const std::function<void ( Node& ) >& aAction );
-
-            DLL void LoopTraverseAncestors ( const std::function<void ( const Node& ) >& aAction ) const;
-
-            DLL void RecursiveTraverseAncestors ( const std::function<void ( Node& ) >& aAction );
-            ///@}
-            ///@name Flag Functions
-            ///@{
-            enum FlagBits
-            {
-                EnabledBit = 1,
-                VisibleBit = 2,
-                AllBits    = 3
-            };
-            enum Flags
-            {
-                Enabled = 0,
-                Visible,
-                FlagCount
-            };
-            /** Enables or disables a set of node flags.
-            @param aFlagBits Flag bits to enable/disable.
-            @param aEnabled true to enable, false to disable.
-            */
-            DLL void SetFlags ( size_t aFlagBits, bool aEnabled = true );
-            /** Enables or disables a single node flag.
-            @param aFlag Flag to enable/disable.
-            @param aEnabled true to enable, false to disable.
-            */
-            DLL void SetFlag ( enum Flags aFlag, bool aEnabled = true );
-            DLL bool IsFlagEnabled ( enum Flags aFlag ) const;
-            ///@}
-            ///@name Transform Functions
-            ///@{
-            DLL const Transform& GetLocalTransform() const;
-            DLL const Transform& GetGlobalTransform() const;
-            DLL void SetLocalTransform ( const Transform& aTransform );
-            DLL void SetGlobalTransform ( const Transform& aTransform );
-            ///@}
-            ///@name Node Name Functions
-            ///@{
-            DLL const std::string& GetName() const;
-            DLL void SetName ( const std::string& aName );
-            ///@}
-        private:
-            friend class Scene;
-            std::string mName{"Node"};
-            Node* mParent{};
-            Scene* mScene{};
-            /** Scene iteration helper.
-                Mutable to allow for constant iterations (EC++ Item 3).*/
-            mutable std::vector<Node>::size_type mIterator{ 0 };
-            std::bitset<sizeof ( size_t ) > mFlags{AllBits};
-            Transform mLocalTransform{};
-            Transform mGlobalTransform{};
-            std::vector<Node> mNodes{};
-        };
-
-        ///@name Scene Functions
-        ///@{
-        DLL Scene ( std::initializer_list<Node> aList );
+        DLL explicit Scene(); // "Explicit Scene"... chuckle
         DLL ~Scene();
-        DLL void Append ( const Node& aNode );
-        DLL void Insert ( size_t aIndex, const Node& aNode );
-        /** Reparent a node at the specified index on this tree's child vector. */
-        DLL void Move ( size_t aIndex, Node&& aNode );
-        DLL void Erase ( std::vector<Node>::size_type aIndex );
-        DLL void Erase ( const Node& aNode );
-        DLL std::vector<Node>::size_type GetChildrenCount() const;
-        DLL const Node& GetChild ( size_t aIndex ) const;
-        DLL Node& GetChild ( size_t aIndex );
-        DLL const Node& operator[] ( const std::size_t aIndex ) const;
-        DLL Node& operator[] ( const std::size_t aIndex );
+        DLL void SetName ( const char* aName );
+        DLL const char* const GetName() const;
+        DLL bool Add ( Node* aNode );
+        DLL bool Insert ( size_t aIndex, Node* aNode );
+        DLL bool Remove ( Node* aNode );
+        DLL bool RemoveByIndex ( size_t aIndex );
+        DLL bool Move ( size_t aIndex, Node* aNode );
+        DLL size_t GetChildrenCount() const;
+        DLL Node* GetChild ( size_t aIndex ) const;
+        DLL size_t GetChildIndex ( const Node* aNode ) const;
+        DLL const Node& operator[] ( const std::size_t index ) const;
+        DLL Node& operator[] ( const std::size_t index );
+        DLL void Update ( const double delta );
         /** @copydoc Node::LoopTraverseDFSPreOrder(std::function<void(Node&) > aAction)*/
         DLL void LoopTraverseDFSPreOrder ( const std::function<void ( Node& ) >& aAction );
         /** @copydoc Node::LoopTraverseDFSPreOrder(
@@ -221,18 +62,23 @@ namespace AeonGames
             const std::function<void ( Node& ) >& aPostamble );
         /** @copydoc Node::LoopTraverseDFSPreOrder(std::function<void(const std::shared_ptr<const Node>&) > aAction) const */
         DLL void LoopTraverseDFSPreOrder ( const std::function<void ( const Node& ) >& aAction ) const;
-        /** @copydoc Node::LoopTraverseDFSPostOrder(std::function<void(const std::shared_ptr<Node>&) > aAction)*/
+        /** @copydoc Node::LoopTraverseDFSPostOrder(std::function<void(const Node*&) > aAction)*/
         DLL void LoopTraverseDFSPostOrder ( const std::function<void ( Node& ) >& aAction );
         /** @copydoc Node::LoopTraverseDFSPostOrder(std::function<void(const std::shared_ptr<const Node>&) > aAction) const */
         DLL void LoopTraverseDFSPostOrder ( const std::function<void ( const Node& ) >& aAction ) const;
-        /** @copydoc Node::RecursiveTraverseDFSPreOrder(std::function<void(const std::shared_ptr<Node>&) > aAction)*/
+        /** @copydoc Node::RecursiveTraverseDFSPreOrder(std::function<void(const Node*&) > aAction)*/
         DLL void RecursiveTraverseDFSPreOrder ( const std::function<void ( Node& ) >& aAction );
-        /** @copydoc Node::RecursiveTraverseDFSPostOrder(std::function<void(const std::shared_ptr<Node>&) > aAction)*/
+        /** @copydoc Node::RecursiveTraverseDFSPostOrder(std::function<void(const Node*&) > aAction)*/
         DLL void RecursiveTraverseDFSPostOrder ( const std::function<void ( Node& ) >& aAction );
-        ///@}
-        DLL std::string Serialize ( bool aAsBinary = true ) const;
+        DLL std::string Serialize ( bool aAsBinary ) const;
+        // Deleted Methods (avoid copy and copy construction)
+        Scene& operator= ( const Scene& ) = delete;
+        Scene ( const Scene& ) = delete;
     private:
-        std::vector<Node> mNodes{};
+        friend class Node;
+        std::string mName;
+        std::vector<Node*> mNodes;
     };
 }
 #endif
+
