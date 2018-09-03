18e09cf879b73fae006209a350fe4b76e18b4370
Rodrigo Hernandez <kwizatz@aeongames.com>
2018-09-02 22:46:31 -0600

    Adding some idea code on how to implement an API for...
    Protocol buffer messages as properties.
    
    This code does nothing since it is not used,
    the idea is to make message wrapper fields
    mutable and move them to the component class.
    
    This is not as easy due to repeated and message fields
    having corner cases to handle, but this code is a start.
    
    The idea is to have each field set and get value functions,
    and make repeated fields be a single field with multiple
    values rather than multiple fields with single values.

--- tools/worldeditor/MessageWrapper.h
+++ tools/worldeditor/MessageWrapper.h
@@ -24,6 +24,7 @@ namespace google
         class Message;
         class Descriptor;
         class FieldDescriptor;
+        class EnumValueDescriptor;
     }
 }
 namespace AeonGames
@@ -46,6 +47,7 @@ namespace AeonGames
             const google::protobuf::FieldDescriptor* GetFieldDescriptor() const;
             const Field* GetParent() const;
             const std::vector<Field>& GetChildren() const;
+            Field& GetField ( const google::protobuf::FieldDescriptor * aFieldDescriptor );
         private:
             google::protobuf::Message* mMessage{};
             const google::protobuf::FieldDescriptor* mFieldDescriptor{};
@@ -60,6 +62,33 @@ namespace AeonGames
         google::protobuf::Message* GetMessagePtr() const;
         const std::vector<Field>& GetFields() const;
         int GetFieldIndex ( const Field* aField ) const;
+        /**@name Message Interface*/
+        /**{*/
+#if 0
+        Field& SetInt32 ( const google::protobuf::FieldDescriptor * aFieldDescriptor, int32_t aValue, int aIndex = 0 );
+        Field& SetInt64 ( const google::protobuf::FieldDescriptor * aFieldDescriptor, int64_t aValue, int aIndex = 0 );
+        Field& SetUInt32 ( const google::protobuf::FieldDescriptor * aFieldDescriptor, uint32_t aValue, int aIndex = 0 );
+        Field& SetUInt64 ( const google::protobuf::FieldDescriptor * aFieldDescriptor, uint64_t aValue, int aIndex = 0 );
+        Field& SetFloat ( const google::protobuf::FieldDescriptor * aFieldDescriptor, float aValue, int aIndex = 0 );
+        Field& SetDouble ( const google::protobuf::FieldDescriptor * aFieldDescriptor, double aValue, int aIndex = 0 );
+        Field& SetBool ( const google::protobuf::FieldDescriptor * aFieldDescriptor, bool aValue, int aIndex = 0 );
+        Field& SetString ( const google::protobuf::FieldDescriptor * aFieldDescriptor, const std::string & aValue, int aIndex = 0 );
+        Field& SetEnum ( const google::protobuf::FieldDescriptor * aFieldDescriptor, const google::protobuf::EnumValueDescriptor * aValue, int aIndex = 0 );
+        Field& SetEnumValue ( const google::protobuf::FieldDescriptor * aFieldDescriptor, int aValue, int aIndex = 0 );
+        Field& AddInt32 ( const google::protobuf::FieldDescriptor * aFieldDescriptor, int32_t aValue );
+        Field& AddInt64 ( const google::protobuf::FieldDescriptor * aFieldDescriptor, int64_t aValue );
+        Field& AddUInt32 ( const google::protobuf::FieldDescriptor * aFieldDescriptor, uint32_t aValue );
+        Field& AddUInt64 ( const google::protobuf::FieldDescriptor * aFieldDescriptor, uint64_t aValue );
+        Field& AddFloat ( const google::protobuf::FieldDescriptor * aFieldDescriptor, float aValue );
+        Field& AddDouble ( const google::protobuf::FieldDescriptor * aFieldDescriptor, double aValue );
+        Field& AddBool ( const google::protobuf::FieldDescriptor * aFieldDescriptor, bool aValue );
+        Field& AddString ( const google::protobuf::FieldDescriptor * aFieldDescriptor, const std::string & aValue );
+        Field& AddEnum ( const google::protobuf::FieldDescriptor * aFieldDescriptor, const google::protobuf::EnumValueDescriptor * aValue );
+        Field& AddEnumValue ( const google::protobuf::FieldDescriptor * aFieldDescriptor, int aValue );
+        Field& AddMessage ( const google::protobuf::FieldDescriptor * aFieldDescriptor );
+#endif
+        Field& GetField ( const google::protobuf::FieldDescriptor * aFieldDescriptor );
+        /**}*/
     private:
         google::protobuf::Message* mMessage{};
         std::vector<Field> mFields{};
