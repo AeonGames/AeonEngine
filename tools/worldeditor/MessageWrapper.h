8771145a1c4f6afb9d898a4ca10545751030daa2
Rodrigo Hernandez <kwizatz@aeongames.com>
2018-09-06 16:35:40 -0600

    Revert "Adding some idea code on how to implement an API for..."
    This path was becoming a roadblock and added more complexity
    than actually needed to be. The world editor worked as expected
    at 5 commits ago, and all I really needed is a way let components
    know their pb message data had changed. I came up with
    many ideas on how to do that, but the design just kept getting
    more and more complicated, not to mention having to maintain
    extra objects with redundant data just to keep track of PB fields.
    This is fine for the editor but not for the runtime.
    
    So, in the end I think the best solution is to clean up the
    the MessageWrapper class (perhaps merge it with the MessageModel
    instead), clean up the SceneWindow class code for message writting
    (move it to the model or the wrapper), and finally, let the component
    expose its PB Message as mutable but add a function to let
    the component know something changed in the message.
    
    This reverts commit 18e09cf879b73fae006209a350fe4b76e18b4370.
    This reverts commit d68aa0d05a98d8d4192f2206fdbc3106e74ed1d0.
    This reverts commit 2b5bb3aba258d368a696e8bbaf8e48aebb9d9d9a.
    This reverts commit 0baea8bc4cfb88402c3f800ca3aa1daad656c97a.
    This reverts commit 509fb5ecc1ea16b7d93d9532d4d8a1eab68867cb.

---
+++ tools/worldeditor/MessageWrapper.h
@@ -0,0 +1,68 @@
+/*
+Copyright (C) 2018 Rodrigo Jose Hernandez Cordoba
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#ifndef AEONGAMES_MESSAGEWRAPPER_H
+#define AEONGAMES_MESSAGEWRAPPER_H
+#include <vector>
+#include <string>
+namespace google
+{
+    namespace protobuf
+    {
+        class Message;
+        class Descriptor;
+        class FieldDescriptor;
+    }
+}
+namespace AeonGames
+{
+    class MessageWrapper
+    {
+    public:
+        class Field
+        {
+        public:
+            Field ( google::protobuf::Message* aMessage, const google::protobuf::FieldDescriptor* aFieldDescriptor, int aRepeatedIndex = 0, Field* aParent = nullptr );
+            Field ( const Field& aField );
+            Field& operator= ( const Field& aField );
+            Field ( const Field&& aField );
+            Field& operator= ( const Field&& aField );
+            int GetIndexAtParent() const;
+            int GetRepeatedIndex() const;
+            std::string GetPrintableName() const;
+            google::protobuf::Message* GetMessagePtr() const;
+            const google::protobuf::FieldDescriptor* GetFieldDescriptor() const;
+            const Field* GetParent() const;
+            const std::vector<Field>& GetChildren() const;
+        private:
+            google::protobuf::Message* mMessage{};
+            const google::protobuf::FieldDescriptor* mFieldDescriptor{};
+            int mRepeatedIndex{};
+            Field* mParent{};
+            std::vector<Field> mChildren{};
+        };
+        MessageWrapper();
+        MessageWrapper ( google::protobuf::Message* aMessage );
+        ~MessageWrapper();
+        void SetMessage ( google::protobuf::Message* aMessage );
+        google::protobuf::Message* GetMessagePtr() const;
+        const std::vector<Field>& GetFields() const;
+        int GetFieldIndex ( const Field* aField ) const;
+    private:
+        google::protobuf::Message* mMessage{};
+        std::vector<Field> mFields{};
+    };
+}
+#endif
