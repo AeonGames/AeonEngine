fcab486727eceff99fc7e2b896f86408af40c4d6
Rodrigo Hernandez <kwizatz@aeongames.com>
2018-10-26 00:04:22 -0600

    Initial code for UniqueResource class.
    This is meant as a variant unique pointer implementation
    in order to be able to store different types of
    objects into a single resource manager.
    
    It should also allow for custom resource type registration
    and creation.

---
+++ tests/UniqueResourceTests.cpp
@@ -0,0 +1,39 @@
+/*
+Copyright (C) 2018 Rodrigo Jose Hernandez Cordoba
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <iostream>
+#include <cstdint>
+#include <string>
+#include "aeongames/UniqueResource.h"
+#include "gtest/gtest.h"
+
+using namespace ::testing;
+namespace AeonGames
+{
+    TEST ( UniqueResource, Constructor )
+    {
+        UniqueResource resource1{new std::string{"Resource 1"}};
+        UniqueResource resource2{std::make_unique<std::string> ( "Resource 2" ) };
+        UniqueResource resource3{std::move ( resource2 ) };
+    }
+
+    TEST ( UniqueResource, Assignment )
+    {
+        UniqueResource resource1 = new std::string{"Resource 1"};
+        UniqueResource resource2 = std::make_unique<std::string> ( "Resource 2" );
+        UniqueResource resource3 = std::move ( resource2 );
+    }
+}
