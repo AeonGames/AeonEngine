16118f7059354f3d0511c1d242446f670b339cf0
Rodrigo Hernandez <kwizatz@aeongames.com>
2019-01-16 21:05:13 -0600

    Added code to handle unsigned long long property values.

--- tools/worldeditor/delegates/PropertyDelegate.cpp
+++ tools/worldeditor/delegates/PropertyDelegate.cpp
@@ -15,54 +15,82 @@ limitations under the License.
 */
 
 #include "PropertyDelegate.h"
+#include <limits>
+#include <QSpinBox>
 
 namespace AeonGames
 {
     PropertyDelegate::PropertyDelegate ( QObject *parent ) : QStyledItemDelegate ( parent ) {}
 
+    template<class T> QWidget *BuildSpinboxEditor ( QWidget* parent )
+    {
+        QSpinBox *editor = new QSpinBox ( parent );
+        editor->setFrame ( false );
+        editor->setMinimum ( std::numeric_limits<T>::min() );
+        editor->setMaximum ( std::numeric_limits<T>::min() );
+        return editor;
+    }
+
     QWidget *PropertyDelegate::createEditor ( QWidget *parent,
             const QStyleOptionViewItem &option,
             const QModelIndex &index ) const
     {
-#if 0
-        QSpinBox *editor = new QSpinBox ( parent );
-        editor->setFrame ( false );
-        editor->setMinimum ( 0 );
-        editor->setMaximum ( 100 );
-
-        return editor;
-#endif
+        QVariant value = index.model()->data ( index, Qt::EditRole );
+        int user_type{value.userType() };
+        switch ( user_type )
+        {
+        case QMetaType::ULongLong:
+            return BuildSpinboxEditor<unsigned long long> ( parent );
+        }
         return QStyledItemDelegate::createEditor ( parent, option, index );
     }
     void PropertyDelegate::setEditorData ( QWidget *editor,
                                            const QModelIndex &index ) const
     {
-#if 0
-        int value = index.model()->data ( index, Qt::EditRole ).toInt();
-
-        QSpinBox *spinBox = static_cast<QSpinBox*> ( editor );
-        spinBox->setValue ( value );
-#endif
+        QVariant value = index.model()->data ( index, Qt::EditRole );
+        int user_type{value.userType() };
+        switch ( user_type )
+        {
+        case QMetaType::ULongLong:
+        {
+            QSpinBox *spinBox = static_cast<QSpinBox*> ( editor );
+            spinBox->setValue ( value.value<unsigned long long>() );
+        }
+        return;
+        }
         QStyledItemDelegate::setEditorData ( editor, index );
     }
     void PropertyDelegate::setModelData ( QWidget *editor, QAbstractItemModel *model,
                                           const QModelIndex &index ) const
     {
-#if 0
-        QSpinBox *spinBox = static_cast<QSpinBox*> ( editor );
-        spinBox->interpretText();
-        int value = spinBox->value();
-
-        model->setData ( index, value, Qt::EditRole );
-#endif
+        QVariant value = index.model()->data ( index, Qt::EditRole );
+        int user_type{value.userType() };
+        switch ( user_type )
+        {
+        case QMetaType::ULongLong:
+        {
+            QSpinBox *spinBox = static_cast<QSpinBox*> ( editor );
+            spinBox->interpretText();
+            unsigned long long value = spinBox->value();
+            model->setData ( index, value, Qt::EditRole );
+        }
+        return;
+        }
         QStyledItemDelegate::setModelData ( editor, model, index );
     }
     void PropertyDelegate::updateEditorGeometry ( QWidget *editor,
             const QStyleOptionViewItem &option, const QModelIndex &index ) const
     {
-#if 0
-        editor->setGeometry ( option.rect );
-#endif
+        QVariant value = index.model()->data ( index, Qt::EditRole );
+        int user_type{value.userType() };
+        switch ( user_type )
+        {
+        case QMetaType::ULongLong:
+        {
+            editor->setGeometry ( option.rect );
+        }
+        return;
+        }
         QStyledItemDelegate::updateEditorGeometry ( editor, option, index );
     }
 }
