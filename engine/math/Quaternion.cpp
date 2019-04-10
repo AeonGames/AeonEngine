83bc78045556690f4ad6c204db60131664fe1365
Rodrigo Hernandez <kwizatz@aeongames.com>
2019-04-10 01:03:57 -0600

    Moved _USE_MATH_DEFINES to be a global command line define.

--- engine/math/Quaternion.cpp
+++ engine/math/Quaternion.cpp
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#define _USE_MATH_DEFINES
 #include "aeongames/Quaternion.h"
 #include "aeongames/Matrix4x4.h"
 #include "aeongames/Matrix3x3.h"
