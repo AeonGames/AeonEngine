1d9e1bec9d22b8784a1357e0bb9188b8687f93f4
Rodrigo Hernandez <kwizatz@aeongames.com>
2019-04-15 00:00:35 -0600

    Added costarican spanish translations to worldeditor.
    Translations might be in there, but I am not sure how to change
    current display language.

---
+++ tools/worldeditor/i18n/es_CR.ts
@@ -0,0 +1,271 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_CR" sourcelanguage="en_US">
+<context>
+    <name>AeonGames::MainWindow</name>
+    <message>
+        <location filename="../MainWindow.cpp" line="49"/>
+        <source>Untitled Scene</source>
+        <translation>Escena sin titulo</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="58"/>
+        <source>Open Scene</source>
+        <translation>Abrir Escena</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="60"/>
+        <location filename="../MainWindow.cpp" line="88"/>
+        <source>Scene Files (*.scn *.txt)</source>
+        <translation>Archivos de Escena</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="86"/>
+        <source>Save Scene</source>
+        <translation>Salvar Escena</translation>
+    </message>
+</context>
+<context>
+    <name>AeonGames::SceneWindow</name>
+    <message>
+        <location filename="../SceneWindow.cpp" line="61"/>
+        <source>Add </source>
+        <translation>Agregar </translation>
+    </message>
+    <message>
+        <location filename="../SceneWindow.cpp" line="63"/>
+        <source> Data</source>
+        <translation>Datos</translation>
+    </message>
+    <message>
+        <location filename="../SceneWindow.cpp" line="66"/>
+        <source>Adds a new component of the specified type to the selected node</source>
+        <translation>Agrega un nuevo componente del tipo especificado al nodo seleccionado</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="14"/>
+        <source>AeonGames World Editor</source>
+        <translation>Editor de Mundos AeonGames</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="49"/>
+        <source>File</source>
+        <translation>Archivo</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="62"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="73"/>
+        <location filename="../ui/MainWindow.ui" line="76"/>
+        <source>Open</source>
+        <translation>Abrir</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="79"/>
+        <source>Ctrl+O</source>
+        <translation>Ctrl+O</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="87"/>
+        <location filename="../ui/MainWindow.ui" line="90"/>
+        <source>Save</source>
+        <translation>Salvar</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="93"/>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+S</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="98"/>
+        <source>Exit</source>
+        <translation>Salir</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="103"/>
+        <source>&amp;New</source>
+        <translation>&amp;Nuevo</translation>
+    </message>
+    <message>
+        <location filename="../ui/MainWindow.ui" line="106"/>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+</context>
+<context>
+    <name>RendererSelectionDialog</name>
+    <message>
+        <location filename="../ui/RendererSelectDialog.ui" line="14"/>
+        <source>Select Renderer</source>
+        <translation>Seleccionar Renderizador</translation>
+    </message>
+    <message>
+        <location filename="../ui/RendererSelectDialog.ui" line="28"/>
+        <source>Renderer</source>
+        <translation>Renderizador</translation>
+    </message>
+</context>
+<context>
+    <name>ResourceDialog</name>
+    <message>
+        <location filename="../ui/ResourcesDialog.ui" line="14"/>
+        <source>Resources</source>
+        <translation>Recursos</translation>
+    </message>
+    <message>
+        <location filename="../ui/ResourcesDialog.ui" line="20"/>
+        <source>Resource Locations:</source>
+        <translation>Localizaciones de Recursos:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ResourcesDialog.ui" line="49"/>
+        <source>Add</source>
+        <translation>Agregar</translation>
+    </message>
+    <message>
+        <location filename="../ui/ResourcesDialog.ui" line="56"/>
+        <source>Remove</source>
+        <translation>Remover</translation>
+    </message>
+    <message>
+        <location filename="../ui/ResourcesDialog.ui" line="63"/>
+        <source>Close</source>
+        <translation>Cerrar</translation>
+    </message>
+</context>
+<context>
+    <name>SceneWindow</name>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="14"/>
+        <source>Scene</source>
+        <translation>Escena</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="58"/>
+        <source>Scenegraph</source>
+        <translation>Grafo de escena</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="104"/>
+        <source>Node Properties</source>
+        <translation>Propiedades de Nodo</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="120"/>
+        <source>Transforms</source>
+        <translation>Transformaciones</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="132"/>
+        <source>Local Transform</source>
+        <translation>Transformacion Local</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="148"/>
+        <location filename="../ui/SceneWindow.ui" line="356"/>
+        <source>Translation</source>
+        <translation>Traslacion</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="198"/>
+        <location filename="../ui/SceneWindow.ui" line="286"/>
+        <source>Rotation</source>
+        <translation>Rotacion</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="251"/>
+        <location filename="../ui/SceneWindow.ui" line="336"/>
+        <source>Scale</source>
+        <translation>Escala</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="267"/>
+        <source>Global Transform</source>
+        <translation>Transformacion Global</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="413"/>
+        <source>Components</source>
+        <translation>Componentes</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="468"/>
+        <source>Add node</source>
+        <translation>Agregar nodo</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="471"/>
+        <source>Add a child node to this scene or node</source>
+        <translation>Agregar nodo hijo a esta escena o nodo</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="480"/>
+        <source>Remove node</source>
+        <translation>Remover nodo</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="483"/>
+        <source>Remove a child node from this scene or node</source>
+        <translation>Remover nodo hijo de esta escena o nodo</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="492"/>
+        <source>Set as camera</source>
+        <translation>Establecer como camara</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="495"/>
+        <source>Set node as initial/default/current camera</source>
+        <translation>Establecer nodo como camara inicial/por defecto/actual</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="504"/>
+        <source>Remove component</source>
+        <translation>Remover Componente</translation>
+    </message>
+    <message>
+        <location filename="../ui/SceneWindow.ui" line="507"/>
+        <source>Remove selected component from node</source>
+        <translation>Remover componente seleccionado del nodo</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../ui/SettingsDialog.ui" line="17"/>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsDialog.ui" line="26"/>
+        <source>OpenGL View</source>
+        <translation>Vista OpenGL</translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsDialog.ui" line="32"/>
+        <source>Background Color:</source>
+        <translation>Color de Fondo:</translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsDialog.ui" line="55"/>
+        <source>Apply</source>
+        <translation>Aplicar</translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsDialog.ui" line="62"/>
+        <source>Revert</source>
+        <translation>Revertir</translation>
+    </message>
+    <message>
+        <location filename="../ui/SettingsDialog.ui" line="69"/>
+        <source>Close</source>
+        <translation>Cerrar</translation>
+    </message>
+</context>
+</TS>
