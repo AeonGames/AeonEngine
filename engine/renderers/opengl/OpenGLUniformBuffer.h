11296681e5bac7713b4d06b78d385109d449f0dd
Rodrigo Hernandez <kwizatz@aeongames.com>
2019-07-23 16:23:30 -0600

    Fixing wrong include guard macro.

--- engine/renderers/opengl/OpenGLUniformBuffer.h
+++ engine/renderers/opengl/OpenGLUniformBuffer.h
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2018 Rodrigo Jose Hernandez Cordoba
+Copyright (C) 2018,2019 Rodrigo Jose Hernandez Cordoba
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,8 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#ifndef AEONGAMES_VULKANUNIFORMBUFFER_H
-#define AEONGAMES_VULKANUNIFORMBUFFER_H
+#ifndef AEONGAMES_OPENGLUNIFORMBUFFER_H
+#define AEONGAMES_OPENGLUNIFORMBUFFER_H
 #include "aeongames/UniformBuffer.h"
 #include "OpenGLBuffer.h"
 namespace AeonGames
