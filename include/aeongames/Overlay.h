5445d091cf41bfcc66cb5c69b05d4b58796b0bca
Rodrigo Hernandez <kwizatz@aeongames.com>
2019-08-04 19:26:32 -0600

    Initial Overlay interface files.

---
+++ include/aeongames/Overlay.h
@@ -0,0 +1,33 @@
+/*
+Copyright (C) 2019 Rodrigo Jose Hernandez Cordoba
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#ifndef AEONGAMES_OVERLAY_H
+#define AEONGAMES_OVERLAY_H
+#include "aeongames/Platform.h"
+#include <cstdint>
+namespace AeonGames
+{
+    class Overlay
+    {
+    public:
+        DLL virtual ~Overlay() = 0;
+        virtual void Resize ( size_t aWidth, size_t aHeight ) = 0;
+        virtual void SetPixels ( void* aBuffer, size_t aBufferSize ) = 0;
+        virtual size_t GetWidth() const = 0;
+        virtual size_t GetHeight() const = 0;
+        virtual size_t GetSize() const = 0;
+    };
+}
+#endif
