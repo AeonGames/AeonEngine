1d24428218bbf7bb7caf74d31d018fa802206c34
Rodrigo Hernandez <kwizatz@aeongames.com>
2019-08-21 19:05:55 -0600

    More refactoring on Material class.
    OpenGL is broken, and Vulkan for some reason is throwing an exception on
    device destruction, but this is not new, may be Intel only.

--- include/aeongames/Material.h
+++ include/aeongames/Material.h
@@ -19,6 +19,7 @@ limitations under the License.
 #include <vector>
 #include <memory>
 #include <variant>
+#include <initializer_list>
 #include "aeongames/Platform.h"
 #include "aeongames/ResourceId.h"
 #include "aeongames/Vector2.h"
@@ -34,8 +35,18 @@ namespace AeonGames
     class Material
     {
     public:
-        using UniformValue = std::variant<uint32_t, int32_t, float, Vector2, Vector3, Vector4, Matrix4x4, ResourceId>;
+        using UniformValue = std::variant<uint32_t, int32_t, float, Vector2, Vector3, Vector4, Matrix4x4>;
         using UniformKeyValue = std::tuple<std::string, UniformValue>;
+        using SamplerKeyValue = std::tuple<std::string, ResourceId>;
+        DLL Material();
+        /// The Copy Contsructor is used for virtual copying.
+        DLL Material ( const Material& aMaterial );
+        /// Assignment operator due to rule of zero/three/five.
+        DLL Material& operator= ( const Material& aMaterial );
+        /// No move assignment allowed
+        Material& operator = ( Material&& ) = delete;
+        /// No move allowed
+        Material ( Material&& ) = delete;
         DLL virtual ~Material() = 0;
         /// Virtual Copy Constructor
         virtual std::unique_ptr<Material> Clone() const = 0;
@@ -45,11 +56,13 @@ namespace AeonGames
         DLL void Load ( const uint32_t aId );
         DLL void Load ( const void* aBuffer, size_t aBufferSize );
         virtual void Load ( const MaterialBuffer& aMaterialBuffer ) = 0;
+        virtual void Load ( std::initializer_list<UniformKeyValue> aUniforms, std::initializer_list<SamplerKeyValue> aSamplers ) = 0;
         virtual void Unload() = 0;
         ///@}
         ///@name Property and Sampler Setters
         ///@{
         virtual void Set ( const std::string& aName, const UniformValue& aValue ) = 0;
+        virtual void SetSampler ( const std::string& aName, const ResourceId& aValue ) = 0;
         ///@}
         ///@name Property and Sampler Getters
         ///@{
@@ -62,6 +75,35 @@ namespace AeonGames
         virtual ResourceId GetSampler ( const std::string& aName ) = 0;
         virtual const std::vector<std::tuple<std::string, ResourceId>>& GetSamplers() const = 0;
         ///@}
+    protected:
+        class UniformVariable
+        {
+        public:
+            UniformVariable ( const std::string& aName, size_t aType, size_t aOffset ) :
+                mName{aName},
+                mType{aType},
+                mOffset{aOffset} {}
+            const std::string& GetName() const
+            {
+                return mName;
+            }
+            size_t GetType()
+            {
+                return mType;
+            }
+            size_t GetOffset()
+            {
+                return mOffset;
+            }
+        private:
+            std::string mName{};
+            size_t mType{};
+            size_t mOffset{};
+        };
+        DLL size_t LoadVariables ( const MaterialBuffer& aMaterialBuffer );
+        DLL void LoadSamplers ( const MaterialBuffer& aMaterialBuffer );
+        std::vector<UniformVariable> mVariables{};
+        std::vector<std::tuple<std::string, ResourceId>> mSamplers{};
     };
 }
 #endif
