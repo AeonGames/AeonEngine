adf623a1d3a32c134f5d88098c4669d430d66dbb
Rodrigo Hernandez <kwizatz@aeongames.com>
2021-07-19 15:14:35 -0600

    Cleaning up include files on OpenGL renderer resource classes.

--- engine/renderers/opengl/OpenGLMaterial.cpp
+++ engine/renderers/opengl/OpenGLMaterial.cpp
@@ -13,12 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <fstream>
-#include <sstream>
-#include <ostream>
-#include <regex>
-#include <utility>
-#include "aeongames/ResourceCache.h"
 #include "aeongames/Material.h"
 #include "aeongames/Texture.h"
 #include "OpenGLFunctions.h"
