84b1cef1d29c5fd5d4f1586158a97d72c491a275
Rodrigo Hernandez <kwizatz@aeongames.com>
2021-09-08 21:29:10 -0600

    Work on Qt Node Editor.
    And some other general adjustments.

---
+++ tools/worldeditor/NodeEditor.h
@@ -0,0 +1,35 @@
+/*
+Copyright (C) 2021 Rodrigo Jose Hernandez Cordoba
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#ifndef AEONGAMES_NODE_EDITOR_H
+#define AEONGAMES_NODE_EDITOR_H
+
+#include "ui_NodeEditor.h"
+
+namespace AeonGames
+{
+    class NodeEditor : public QWidget, public Ui::NodeEditor
+    {
+        Q_OBJECT
+    public:
+        NodeEditor ( QWidget *parent = Q_NULLPTR, Qt::WindowFlags f = Qt::WindowFlags() );
+        ~NodeEditor();
+    private slots:
+    signals:
+    private:
+        QGraphicsScene* mScene;
+    };
+}
+#endif
