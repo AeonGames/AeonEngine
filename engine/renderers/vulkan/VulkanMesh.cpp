4d5fb1f744f471d21283982c57f3bde714e3defa
Rodrigo Hernandez <kwizatz@aeongames.com>
2023-07-18 19:47:20 -0600

    Work on Vulkan Meshes.
    Mesh vertex stride is needed on pipeline and there is currently no way
    to get that data from the shader code alone, so the Vulkan Pipeline is not working yet.
    
    Pipeline needs a rework anyway to better support different kinds of
    shaders, not just mesh rendering.

--- engine/renderers/vulkan/VulkanMesh.cpp
+++ engine/renderers/vulkan/VulkanMesh.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2017-2019,2021 Rodrigo Jose Hernandez Cordoba
+Copyright (C) 2017-2019,2021,2023 Rodrigo Jose Hernandez Cordoba
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -34,87 +34,32 @@ namespace AeonGames
     VulkanMesh::VulkanMesh ( const VulkanRenderer& aVulkanRenderer, const Mesh& aMesh ) :
         mVulkanRenderer {aVulkanRenderer}, mMesh{&aMesh}, mMeshBuffer { mVulkanRenderer }
     {
-        // Vulkan Specific code
-        const VkDeviceSize vertex_buffer_size = ( sizeof ( Vertex ) * aMesh.GetVertexCount() );
-        // We need to expand 1 byte indices to 2 since they're not supported on Vulkan
-        const VkDeviceSize index_buffer_size = aMesh.GetIndexBuffer().size() * ( ( aMesh.GetIndexSize() == 1 ) ? 2 : 1 );
-        VkDeviceSize buffer_size = vertex_buffer_size + index_buffer_size;
-        VkBufferUsageFlags buffer_usage = ( ( aMesh.GetVertexCount() ) ? VK_BUFFER_USAGE_VERTEX_BUFFER_BIT : 0 ) | ( ( aMesh.GetIndexCount() ) ? VK_BUFFER_USAGE_INDEX_BUFFER_BIT : 0 );
+        const VkDeviceSize buffer_size{ aMesh.GetVertexCount() + ( aMesh.GetIndexBuffer().size() * ( aMesh.GetIndexSize() == 1 ? 2 : 1 ) ) };
+        const VkBufferUsageFlags buffer_usage {static_cast<VkBufferUsageFlags> ( ( aMesh.GetVertexCount() ? VK_BUFFER_USAGE_VERTEX_BUFFER_BIT : 0 ) | ( aMesh.GetIndexCount() ? VK_BUFFER_USAGE_INDEX_BUFFER_BIT : 0 ) ) };
+
         mMeshBuffer.Initialize ( buffer_size, buffer_usage | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT );
-        std::vector<uint8_t> buffer ( buffer_size );
+
         if ( aMesh.GetVertexCount() )
         {
-            auto* vertices = reinterpret_cast<Vertex*> ( buffer.data() );
-            memset ( vertices, 0, vertex_buffer_size );
-            uintptr_t offset = 0;
-            for ( uint32_t j = 0; j < aMesh.GetVertexCount(); ++j )
-            {
-                if ( aMesh.GetVertexFlags() & Mesh::POSITION_BIT )
-                {
-                    memcpy ( vertices[j].position, aMesh.GetVertexBuffer().data() + offset, sizeof ( Vertex::position ) );
-                    offset += sizeof ( Vertex::position );
-                }
-
-                if ( aMesh.GetVertexFlags() & Mesh::NORMAL_BIT )
-                {
-                    memcpy ( vertices[j].normal, aMesh.GetVertexBuffer().data() + offset, sizeof ( Vertex::normal ) );
-                    offset += sizeof ( Vertex::normal );
-                }
-
-                if ( aMesh.GetVertexFlags() & Mesh::TANGENT_BIT )
-                {
-                    memcpy ( vertices[j].tangent, aMesh.GetVertexBuffer().data() + offset, sizeof ( Vertex::tangent ) );
-                    offset += sizeof ( Vertex::tangent );
-                }
-
-                if ( aMesh.GetVertexFlags() & Mesh::BITANGENT_BIT )
-                {
-                    memcpy ( vertices[j].bitangent, aMesh.GetVertexBuffer().data() + offset, sizeof ( Vertex::bitangent ) );
-                    offset += sizeof ( Vertex::bitangent );
-                }
-
-                if ( aMesh.GetVertexFlags() & Mesh::UV_BIT )
-                {
-                    memcpy ( vertices[j].uv, aMesh.GetVertexBuffer().data() + offset, sizeof ( Vertex::uv ) );
-                    offset += sizeof ( Vertex::uv );
-                }
-
-                if ( aMesh.GetVertexFlags() & Mesh::WEIGHT_IDX_BIT )
-                {
-                    memcpy ( vertices[j].weight_indices, aMesh.GetVertexBuffer().data() + offset, sizeof ( Vertex::weight_indices ) );
-                    offset += sizeof ( Vertex::weight_indices );
-                }
-
-                if ( aMesh.GetVertexFlags() & Mesh::WEIGHT_BIT )
-                {
-                    memcpy ( vertices[j].weight_influences, aMesh.GetVertexBuffer().data() + offset, sizeof ( Vertex::weight_influences ) );
-                    offset += sizeof ( Vertex::weight_influences );
-                }
-
-                if ( aMesh.GetVertexFlags() & Mesh::COLOR_BIT )
-                {
-                    memcpy ( vertices[j].color, aMesh.GetVertexBuffer().data() + offset, sizeof ( Vertex::color ) );
-                    offset += sizeof ( Vertex::color );
-                }
-            }
+            mMeshBuffer.WriteMemory ( 0, aMesh.GetVertexBuffer().size(), aMesh.GetVertexBuffer().data() );
         }
         if ( aMesh.GetIndexCount() )
         {
-            void* data = buffer.data() + vertex_buffer_size;
             if ( aMesh.GetIndexSize() != 1 )
             {
-                memcpy ( data, aMesh.GetIndexBuffer().data(), aMesh.GetIndexBuffer().size() );
+                mMeshBuffer.WriteMemory ( aMesh.GetVertexBuffer().size(), aMesh.GetIndexBuffer().size(), aMesh.GetIndexBuffer().data() );
             }
             else
             {
-                /**@note upcast to 16 bit indices.*/
-                for ( size_t j = 0; j < aMesh.GetIndexCount(); ++j )
+                std::vector<uint16_t> buffer{};
+                buffer.resize ( aMesh.GetIndexCount() );
+                for ( size_t i = 0; i < aMesh.GetIndexCount(); ++i )
                 {
-                    ( reinterpret_cast<uint16_t*> ( data ) [j] ) = aMesh.GetIndexBuffer() [j];
+                    buffer[i] = aMesh.GetIndexBuffer() [i];
                 }
+                mMeshBuffer.WriteMemory ( aMesh.GetVertexBuffer().size(), buffer.size() * sizeof ( uint16_t ), aMesh.GetIndexBuffer().data() );
             }
         }
-        mMeshBuffer.WriteMemory ( 0, buffer.size(), buffer.data() );
     }
 
     VulkanMesh::~VulkanMesh()
@@ -148,7 +93,7 @@ namespace AeonGames
         if ( mMesh->GetIndexCount() )
         {
             vkCmdBindIndexBuffer ( aVkCommandBuffer,
-                                   mMeshBuffer.GetBuffer(), ( sizeof ( Vertex ) * mMesh->GetVertexCount() ),
+                                   mMeshBuffer.GetBuffer(), mMesh->GetVertexBuffer().size(),
                                    GetIndexType ( mMesh ) );
         }
     }
