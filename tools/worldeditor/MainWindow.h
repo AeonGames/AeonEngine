affd0fbc2c917f597eb58fae2a04f6c6681bd069
Rodrigo Hernandez <kwizatz@aeongames.com>
2024-09-30 22:15:19 -0600

    Fixed weird scaling factor on world editor scenes...
    Also removed FindGLslang.cmake and FindVulkan.cmake since
    these now come bundled with glvalidator and cmake respectively.

--- tools/worldeditor/MainWindow.h
+++ tools/worldeditor/MainWindow.h
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2016-2019,2021,2022 Rodrigo Jose Hernandez Cordoba
+Copyright (C) 2016-2019,2021,2022,2024 Rodrigo Jose Hernandez Cordoba
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -24,7 +24,6 @@ namespace AeonGames
     class NodeEditor;
     class CameraSettings;
     class Renderer;
-    class EngineWindow;
     class MainWindow : public QMainWindow
     {
         Q_OBJECT
