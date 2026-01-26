5aa1c10d9e5364e8e8787310f0b4c12bf5ed92ea
Rodrigo Hernandez <kwizatz@aeongames.com>
2026-01-25 18:10:49 -0600

    Added source blend file for aerin and moved textures to assets directory
    Use cmake target decode_aerin to decode the assets
    and target install_aerin to copy the textures into the game directory.

--- cmake/base64.cmake
+++ cmake/base64.cmake
@@ -13,19 +13,20 @@
 # the License.
 
 # Function to add a custom target for decoding base64 files using aeontool
-# Usage: add_b64_decode_target(target_name extension input_file1 [input_file2 ...])
-# The output files will have the same path as input files but with the new extension
-function(add_b64_decode_target target_name extension)
+# Usage: add_b64_decode_target(target_name input_file1 [input_file2 ...])
+# The output files will have .b64 removed from the input file names
+function(add_b64_decode_target target_name)
   set(input_files ${ARGN})
   set(output_files "")
   set(decode_commands "")
   set(output_dirs "")
   
   foreach(input_file ${input_files})
-    # Get the file path without extension and replace with new extension
+    # Remove .b64 extension from the input file to get the output file
     get_filename_component(file_dir "${input_file}" DIRECTORY)
-    get_filename_component(file_name "${input_file}" NAME_WE)
-    set(output_file "${file_dir}/${file_name}.${extension}")
+    get_filename_component(file_name "${input_file}" NAME)
+    string(REGEX REPLACE "\\.b64$" "" output_name "${file_name}")
+    set(output_file "${file_dir}/${output_name}")
     
     list(APPEND output_files "${output_file}")
     list(APPEND output_dirs "${file_dir}")
@@ -52,5 +53,48 @@ function(add_b64_decode_target target_name extension)
     BYPRODUCTS ${output_files}
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     COMMENT "Decoding base64 files to .${extension} format"
+  )
+endfunction()
+
+# Function to add a custom target for encoding files to base64 using aeontool
+# Usage: add_b64_encode_target(target_name extension input_file1 [input_file2 ...])
+# The output files will have the same path as input files but with the new extension
+function(add_b64_encode_target target_name extension)
+  set(input_files ${ARGN})
+  set(output_files "")
+  set(encode_commands "")
+  set(output_dirs "")
+  
+  foreach(input_file ${input_files})
+    # Get the file path without extension and replace with new extension
+    get_filename_component(file_dir "${input_file}" DIRECTORY)
+    get_filename_component(file_name "${input_file}" NAME_WE)
+    set(output_file "${file_dir}/${file_name}.${extension}")
+    
+    list(APPEND output_files "${output_file}")
+    list(APPEND output_dirs "${file_dir}")
+    list(APPEND encode_commands
+      COMMAND ${CMAKE_COMMAND} -E echo "Encoding: ${input_file}"
+      COMMAND $<TARGET_FILE:aeontool> base64 encode -i "${input_file}" -o "${output_file}"
+    )
+  endforeach()
+  
+  # Remove duplicate directories
+  list(REMOVE_DUPLICATES output_dirs)
+  
+  # Create directory commands
+  set(mkdir_commands "")
+  foreach(dir ${output_dirs})
+    list(APPEND mkdir_commands COMMAND ${CMAKE_COMMAND} -E make_directory "${dir}")
+  endforeach()
+  
+  add_custom_target(
+    ${target_name}
+    ${mkdir_commands}
+    ${encode_commands}
+    DEPENDS aeontool ${input_files}
+    BYPRODUCTS ${output_files}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    COMMENT "Encoding files to base64 .${extension} format"
   )
 endfunction()
