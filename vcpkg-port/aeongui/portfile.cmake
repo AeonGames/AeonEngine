142c950b6ebf7dea5cbb388d1b24c35e7d99a6cd
Rodrigo Hernandez <kwizatz@aeongames.com>
2026-04-04 23:06:19 -0600

    Use VCPKG port overlay for AeonGUI on MSVC build.

---
+++ vcpkg-port/aeongui/portfile.cmake
@@ -0,0 +1,21 @@
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL https://github.com/AeonGames/AeonGUI.git
+    REF c8729a430c4de102b982fe499af37dd99f2bddd0
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DAEONGUI_BACKEND=Cairo
+        -DBUILD_UNIT_TESTS=OFF
+)
+
+vcpkg_cmake_build()
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME aeongui CONFIG_PATH share/aeongui)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
