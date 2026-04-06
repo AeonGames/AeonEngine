458174c3c9be18e27bf6b2530af33c95689ab7fb
Rodrigo Hernandez <kwizatz@aeongames.com>
2026-04-05 19:21:30 -0600

    Bump AeonGUI version, need the crash on resize fix.

--- vcpkg-port/aeongui/portfile.cmake
+++ vcpkg-port/aeongui/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/AeonGames/AeonGUI.git
-    REF 86a2e676c61acdf06b15d3db2f394e9bdb73cc52
+    REF 8a36d5dfaa4cae3793dc00c7285e527c4ba88b4b
 )
 
 vcpkg_cmake_configure(
